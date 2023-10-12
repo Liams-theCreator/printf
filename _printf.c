0e5ded76aab069aa2e6bc5df4aea0cf553b8acfd
AadelAferyad <animevineu@gmail.com>
2023-10-12 21:24:06 +0100

    first approche

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+
+int _printf(const char *format, ...)
+{
+	fr list[] = {
+		{"%c", print_char},
+		{"%s", print_string},
+		{NULL, NULL}
+	};
+	
+
+}
--- main.h
+++ main.h
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * struct format - Struct format.
+ *
+ * @sp: The format.
+ * @f: The function associated.
+ */
 
+typedef struct format
+{
+	char *sp;
+	int (*f)(va_list arg);
+
+} fr;
 int _printf(const char *format, ...);
+
 #endif
